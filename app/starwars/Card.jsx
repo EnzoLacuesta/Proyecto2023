929bf9549342153e810be6536da4ae20c66199e2
Enzo Lacuesta <enzolacuesta1996@gmail.com>
2023-06-22 11:28:50 -0300

    Commit sarasa

--- app/starwars/Card.jsx
+++ app/starwars/Card.jsx
@@ -1,11 +1,14 @@
 'use client'
 import React, { useEffect, useState } from 'react'
+import style from './SW.module.css'
 
 const API_SW = 'https://swapi.dev/api/people'
 
 const Card = () => {
 
     const [PeopleSW, setPeopleSw] = useState([])
+    const [search, setSearch] = useState ('');
+    
 
     useEffect(()=>{
         fetch(API_SW)
@@ -17,13 +20,24 @@ const Card = () => {
 
   return (
     <div>
-        {PeopleSW.map(data =>(
-            <div key={data.name}>
-                {data.name}
-            </div>
-        ))}
+        <input 
+            type="text" 
+            placeholder='Busca tu personaje favorito' 
+            className={style.inpSW}
+            value = {search} 
+            onChange = {handleChange}/>
+
+        <div className={style.ContCard}>        
+            {PeopleSW.map(data =>(
+                <div key={data.name} className={style.Card}>
+                    <p>{data.name}</p>
+                    <p>{data.gender}</p>               
+                </div>
+                ))}
         
+        </div>
     </div>
+
   )
 }
 
--- app/starwars/page.jsx
+++ app/starwars/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import style from '../page.module.css'
+import style from './SW.module.css'
 import Card from './Card'
 const page = () => {
   return (
-    <div className={style.centrar}>
-        Star wars
+    <div className={style.SW}>
+        <div className={style.SW}>
+            <h1> Star wars</h1>
+        </div>  
         <Card></Card>
     </div>
   )
